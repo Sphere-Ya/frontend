3c0f65aa50ed4b5c945ceafa36289909075f3a5b
FreyrsKept <120368406+FreyrsKept@users.noreply.github.com>
2024-04-09 23:13:11 +0700

    Raw event markup

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.scss';
+import { EventPage } from '../EventPage/EventPage';
 
 import Main from '../Main/Main';
 import Header from '../Header/Header';
 
 function App() {
 
-  return (
-      <Routes>
-          <Route
-            index
-            element={
-              <Main>
-                <Header></Header>
-              </Main>
-            }
-          />
-      </Routes>
-  );
+	return (
+		<Routes>
+			<Route
+				index
+				element={
+					<Main>
+						<Header></Header>
+					</Main>
+				}
+			/>
+			<Route path='/event' element={<EventPage />} />
+		</Routes>
+	);
 }
 
 export default App;
