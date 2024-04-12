f3733fd049310b2a6b86f1d77422a02eaf131f36
Denis Manaev <manaewd@yandex.ru>
2024-04-13 00:01:47 +0700

    add styles header & footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,13 +5,13 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <div>
+      <div className="footer__menu">
         <div className="footer__wrapper">
           <Logo />
           <p className="footer__title">Яндекс.Сфера</p>
         </div>
-        <div>
-          <p className="">2024</p>
+        <div className="header__footer-container">
+          <p className="footer__subtitle">2024</p>
         </div>
       </div>
     </footer>
