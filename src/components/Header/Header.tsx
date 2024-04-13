26f364b63eac5cc5447ce78a93cd5913a88136b3
FreyrsKept <120368406+FreyrsKept@users.noreply.github.com>
2024-04-13 15:40:32 +0700

    fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,24 @@
 import React from "react";
-import "./Header.scss"
+import { Link } from "react-router-dom";
+import "./Header.css"
 import Logo from "../Logo/Logo";
 
 function Header() {
 	return (
 		<>
 			<header className="header">
-				<div className="header__wrapper">
-					<Logo />
-					<p className="header__title">Яндекс.Сфера</p>
+				<div className="header__menu">
+					<div className="header__wrapper">
+						<Logo />
+						<p className="header__title">Яндекс.Сфера</p>
+					</div>
+					<div className="header__login-container">
+						<Link to="/" className="header__login">
+							{" "}
+							Войти
+						</Link>
+					</div>
 				</div>
-					<button className="header__button"></button>
 			</header>
 		</>
 	)
